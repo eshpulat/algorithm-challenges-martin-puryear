978e8690fa7aeab46a657766b006a36a3352d08e
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-10-01 17:23:49 -0400

    finish Arrays and For loop

--- chapter-01/arrays-and-forloop.js
+++ chapter-01/arrays-and-forloop.js
@@ -50,3 +50,139 @@ function secondToLast(arr) {
 }
 
 console.log(secondToLast([2, 4, 3, 7, 1, 1, 9]));
+
+// 4 -- Double vision
+
+function double(arr) {
+    let doubleArray = [];
+    for (char of arr) {
+        doubleArray.push(char * 2);
+    }
+
+    return doubleArray;
+}
+
+console.log(double([1, 2, 3]));
+
+// 5 -- Count Positives
+
+function countPositives(arr) {
+    let count = 0;
+
+    for (char of arr) {
+        if (char > 0) {
+            count += char;
+        }
+    }
+    arr.push(count);
+    return arr;
+}
+
+console.log(countPositives([-1, 1, 1, 1]));
+
+// 6 -- Evens and Odds ????????
+
+// function thatsOdd()
+
+// 7 -- increment the seconds
+
+function incrementTheSeconds(arr) {
+    let oddNum = [];
+    for (char of arr) {
+        if (char % 2 !== 0) {
+            oddNum.push(char);
+        }
+    }
+    return oddNum;
+}
+
+console.log(incrementTheSeconds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// 8 -- Previous length
+
+function previouslength(arr) {
+    let strlength = [];
+    for (let i = 0; i < arr.length; i++) {
+        strlength.push(arr[i].length);
+    }
+    return strlength;
+}
+
+console.log(previouslength(["string", "number", "is"]));
+
+// 9 -- add seven to most
+
+function addSeven(arr) {
+    let mostSeven = [];
+    for (char of arr) {
+        mostSeven.push(char + 7);
+    }
+
+    return mostSeven;
+}
+
+console.log(addSeven([1, 2, 3, 4, 5, 6]));
+
+// 10 -- reverse array
+
+function reverseNumber(arr) {
+    const reversed = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+
+    return reversed;
+}
+
+console.log(reverseNumber([3, 1, 6, 4, 2]));
+
+// 11 -- Outlook negative
+
+function negativeNumber(arr) {
+    let makeNegative = [];
+
+    for (char of arr) {
+        if (char < 0) {
+            makeNegative.push(char);
+        } else {
+            makeNegative.push(-char);
+        }
+    }
+
+    return makeNegative;
+}
+
+console.log(negativeNumber([1, -3, 8]));
+
+// 12 -- Always hungry
+
+function alwaysHungry(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "food") {
+            result.push("yummy");
+        } else {
+            result.push("I'm hungry");
+        }
+    }
+    return result;
+}
+
+console.log(alwaysHungry(["is", "food", "no", "yes", "food"]));
+
+// 13 -- Swap toward the center ??????
+
+// 14 -- scal the array
+
+function scalTheArray(arr, num) {
+    let result = [];
+
+    for (char of arr) {
+        result.push(char * num);
+    }
+
+    return result;
+}
+
+console.log(scalTheArray([1, 2, 3, 4], 2));
