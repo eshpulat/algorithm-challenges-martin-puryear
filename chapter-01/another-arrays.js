d972e7fea1b14c0f01fab43121a36ac23dbfb8bc
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-10-01 18:00:42 -0400

    Add T-diagram

---
+++ chapter-01/another-arrays.js
@@ -0,0 +1,15 @@
+// 1 -- only keep the last few
+
+function keepTheLastFew(arr, x) {
+    let result = arr.slice(x);
+
+    return result;
+}
+
+console.log(keepTheLastFew([1, 2, 3, 4, 5, 6, 7], 4));
+
+// 2 -- Math help ????????
+
+// 3 -- poor kenny ??????
+
+// 4 -- what really happened
